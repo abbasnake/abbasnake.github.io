ad940d4e7734c133801e52edb00358c5837bf36e
abbasnake <mr.rihards.ozols@gmail.com>
2017-12-08 21:52:09 +0200

    more skeleton

--- js/grafiks.js
+++ js/grafiks.js
@@ -1,5 +1,13 @@
-$(".calendar").on("click", ".calendar__entry__set", function(){
-    $(this).parent().fadeOut(500, function(){
+
+$(".calendar").on("click", ".calendar__entry--remove", function(){
+    $(this).parent().parent().fadeOut(500, function(){
         $(this).remove();
     })
+});
+
+$(".calendar").on("click", ".calendar--add", function(){
+    $(".calendar").append(
+        "<div class='calendar__date row'><div class='calendar__entry col-md-1'>01</div><div class='calendar__entry col-md-1<div class='calendar__entry col-md-1'>I</div><div class='calendar__entry col-md-2'>Klavigo</div><div class='calendar__entry col-md-2'>JZ</div><div class='calendar__entry col-md-1'><div class='row'><div class='calendar__entry__workers col-md-6'>R</div><div class='calendar__entry__workers col-md-6'>N</div></div></div><div class='calendar__entry col-md-1'>16:00-21:00</div><div class='calendar__entry col-md-2'>paņemt defibrilatoru</div><div class='calendar__entry col-md-1'><button type='button' class='calendar__entry--remove'>X</button></div></div>"
+        );
+
 });
