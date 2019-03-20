fb082a8c4caeed4682c4310c0a5b441da78b07fb
abbasnake <mr.rihards.ozols@gmail.com>
2019-03-20 19:48:56 +0200

    timer update

---
+++ CustomIntervalTimer/dist/precache-manifest.33fb58ee90e8633d353486d5fe3bd65b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a1c4d247d791273aeebae8c2cefee8f",
+    "url": "https://abbasnake.github.io/CustomIntervalTimer/dist/media/beep.9a1c4d24.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://abbasnake.github.io/CustomIntervalTimer/dist/robots.txt"
+  },
+  {
+    "revision": "304b4261cabb1a93cecdacd1406edc92",
+    "url": "https://abbasnake.github.io/CustomIntervalTimer/dist/media/whistle3x.304b4261.mp3"
+  },
+  {
+    "revision": "754038c1eddd59dcb27acb124c056129",
+    "url": "https://abbasnake.github.io/CustomIntervalTimer/dist/media/whistle.754038c1.mp3"
+  },
+  {
+    "revision": "10362e92b34de42560e9",
+    "url": "https://abbasnake.github.io/CustomIntervalTimer/dist/js/run.c873981e.js"
+  },
+  {
+    "revision": "68a3f7c3fc83d79dd589",
+    "url": "https://abbasnake.github.io/CustomIntervalTimer/dist/js/chunk-vendors.12b27295.js"
+  },
+  {
+    "revision": "c3e9b6b55e8854f26ec6",
+    "url": "https://abbasnake.github.io/CustomIntervalTimer/dist/js/app.2c02b192.js"
+  },
+  {
+    "revision": "d7bb9ddd605b078fd657c07da0873b71",
+    "url": "https://abbasnake.github.io/CustomIntervalTimer/dist/index.html"
+  },
+  {
+    "revision": "18c8c580b8a0b2b7a7e392c5a20ada52",
+    "url": "https://abbasnake.github.io/CustomIntervalTimer/dist/fonts/SairaCondensed-Regular.18c8c580.ttf"
+  },
+  {
+    "revision": "10362e92b34de42560e9",
+    "url": "https://abbasnake.github.io/CustomIntervalTimer/dist/css/run.34344205.css"
+  },
+  {
+    "revision": "c3e9b6b55e8854f26ec6",
+    "url": "https://abbasnake.github.io/CustomIntervalTimer/dist/css/app.07391839.css"
+  }
+];
--- CustomIntervalTimer/dist/service-worker.js
+++ CustomIntervalTimer/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://abbasnake.github.io/CustomIntervalTimer/dist/precache-manifest.851f02132d2f4513a3a8425ce02a94c2.js"
+  "https://abbasnake.github.io/CustomIntervalTimer/dist/precache-manifest.33fb58ee90e8633d353486d5fe3bd65b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "custom-hiit"});
