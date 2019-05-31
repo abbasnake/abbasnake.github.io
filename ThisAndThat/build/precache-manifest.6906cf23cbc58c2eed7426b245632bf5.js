cd5766699628888df15531657d49182fa0f264d8
abbasnake <mr.rihards.ozols@gmail.com>
2019-05-31 15:01:33 +0300

    ThisAndThat init

---
+++ ThisAndThat/build/precache-manifest.6906cf23cbc58c2eed7426b245632bf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76be7835e0cd7eb8fdfdc50aa5b3cdd",
+    "url": "/ThisAndThat/build/index.html"
+  },
+  {
+    "revision": "44eb8c81998a72a8d574",
+    "url": "/ThisAndThat/build/static/css/main.356d8705.chunk.css"
+  },
+  {
+    "revision": "fc18f110d5c6ddfd18f0",
+    "url": "/ThisAndThat/build/static/js/2.964bbaaf.chunk.js"
+  },
+  {
+    "revision": "44eb8c81998a72a8d574",
+    "url": "/ThisAndThat/build/static/js/main.dc40e2f8.chunk.js"
+  },
+  {
+    "revision": "0137dd617f935aaa5b9f",
+    "url": "/ThisAndThat/build/static/js/runtime~main.58543b42.js"
+  }
+]);
